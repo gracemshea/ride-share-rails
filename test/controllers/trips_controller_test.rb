5b7e813ef8c129e0af26c6cb24105323def3a574
Grace Shea <grace.m.shea@gmail.com>
2019-04-16 12:35:08 -0700

    stopping point, share with Jansen :)

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,15 +2,44 @@ require "test_helper"
 
 describe TripsController do
   describe "show" do
-    # Your tests go here
+    it "can get a valid trip" do
+      # Act
+      get trip_path(trip.id)
+
+      # Assert
+      must_respond_with :success
   end
 
   describe "edit" do
-    # Your tests go here
+    it "can get the edit page for an existing trip" do
+      trip = Trip.first
+      get edit_trip_path(trip)
+    end
+
+    it "will respond with redirect when attempting to edit a nonexistant trip" do
+      get edit_trip_path(-1)
+
+      # Assert
+      must_respond_with :redirect
+      expect(flash[:error]).must_equal "Could not find trip with id: -1"
+    end
   end
 
   describe "update" do
-    # Your tests go here
+    it "can update an existing trip" do
+      # Arrange
+      trip_id = Trip.last.id
+      trip_hash = {
+        trip: {
+          passenger_id: 1,
+          driver_id: 1,
+          date: "20190416",
+          rating: 5,
+          cost: 15.75,
+        },
+      }
+
+      # not finished!
   end
 
   describe "create" do
