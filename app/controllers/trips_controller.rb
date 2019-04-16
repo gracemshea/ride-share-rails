7a0ebc86e0ae8db0382f9adad17d006a807dd93d
Grace Shea <grace.m.shea@gmail.com>
2019-04-16 12:01:10 -0700

    added to trips_controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,19 +4,32 @@ class TripsController < ApplicationController
   end
 
   def show
-    @trip = Trip.find(params[:id])
+    @trip = Trip.find_by(id: params[:id])
+    if !@trip
+      redirect_to trips_path(@trip), :flash => { :error => "Could not find trip with id: #{params[:id]}" }
+    end
   end
 
-  def new
-    @trip = Trip.new
-  end
+  # Do we even need new and create??
+
+  # def create
+  #   @trip = Trip.new(trip_params)
+  #   if @trip.save
+  #     redirect_to trip_path(@trip.id), { :flash => { :success => "Trip has been added" } }
+  #   else
+  #     redirect_to :new, :flash => { :error => "Failed to add trip" }
+  #   end
+  # end
+
+  # def new
+  #   @trip = Trip.new
+  # end
 
   def edit
     @trip = Trip.find(params[:id])
-  end
-
-  def create
-    @trip = Trip.new(trip_params)
+    if !@trip
+      redirect_to trips_path, :flash => { :error => "Could not find trip with id: #{params[:id]}" }
+    end
   end
 
   def update
