470162ea3f2d59748da0c5440351dc3a5ce85ac0
Grace Shea <grace.m.shea@gmail.com>
2019-04-16 16:47:00 -0700

    improvements made to views for trip, passenger, and homepage. syntax corrections in tests and controllers

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,9 @@
 class HomepagesController < ApplicationController
   def index
+    it "can get the homepage" do
+      get root_path
+
+      must_respond_with :success
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "homepages#index"
 
-  # Trips
   resources :trips
-
-  # Drivers
-  resources :drivers, only: [:index, :show]
-
-  # Passengers
-
-  # Home??
-
+  resources :drivers
+  resources :passengers
 end
