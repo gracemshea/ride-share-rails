394e893f8bb14f37f8dd43f617d2dcc74065dea9
Jansen <v.jansen.martin@gmail.com>
2019-04-16 11:58:39 -0700

    Add comments dividing routes.rb based on models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Trips
+  resources :trips
+
+  # Drivers
+
+  # Passengers
+
+  # Home??
+
 end
