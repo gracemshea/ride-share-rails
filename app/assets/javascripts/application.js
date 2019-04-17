485c517c084c6fe9712c218c3c66b35155339573
Grace Shea <grace.m.shea@gmail.com>
2019-04-17 09:05:22 -0700

    started styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,50 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+// = require rails-ujs
+// = require activestorage
+// = require turbolinks
+// = require_tree .
+
+
+// THE FOLLOWING IS NO LONGER IN PLAY
+
+// const words = ["Welcome to TRANSport!","(a bigot-free riding app)","Click 'passenger' or 'driver' to see all your options!"];
+// let i = 0;
+// let timer;
+
+// function typingEffect() {
+// 	let word = words[i].split("");
+// 	var loopTyping = function() {
+// 		if (word.length > 0) {
+// 			document.getElementById('word').innerHTML += word.shift();
+// 		} else {
+// 			deletingEffect();
+// 			return false;
+// 		};
+// 		timer = setTimeout(loopTyping, 100);
+// 	};
+// 	loopTyping();
+// };
+
+// function deletingEffect() {
+// 	let word = words[i].split("");
+// 	var loopDeleting = function() {
+// 		if (word.length > 0) {
+// 			word.pop();
+// 			document.getElementById('word').innerHTML = word.join("");
+// 		} else {
+// 			if (words.length > (i + 1)) {
+// 				i++;
+// 			} else {
+// 				i = 0;
+// 			};
+// 			typingEffect();
+// 			return false;
+// 		};
+// 		timer = setTimeout(loopDeleting, 50);
+// 	};
+// 	loopDeleting();
+// };
+
+// typingEffect();
