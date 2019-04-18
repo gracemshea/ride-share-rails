070fc7977f798f8e8ee98477c12e7f807983c736
Jansen <v.jansen.martin@gmail.com>
2019-04-18 06:46:57 -0700

    Add 'average_rating' business logic to model; Modify view for drivers to reflect the changes

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,15 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
+
+  # Average rating for a driver
+  def average_rating
+    return "N/A" if self.trips = []
+    ratings = self.trips.map do |trip|
+      trip.rating
+    end
+
+    average_rating = "%.1f" % (ratings.reduce(:+) / ratings.length.to_f)
+    return average_rating
+  end
 end
