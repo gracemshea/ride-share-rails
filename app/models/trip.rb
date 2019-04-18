4a2550bd579411c76b5287f843edcc4eadabed7e
Grace Shea <grace.m.shea@gmail.com>
2019-04-17 20:47:54 -0700

    trip model updated

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,28 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+
+  validates :driver_id, presence: true
+  validates :passenger_id, presence: true
+  validates :date, presence: true
+  validates :rating, presence: true
+  validates :cost, presence: true
+
+  def driver_name
+    driver = self.driver
+    if driver
+      return driver.name
+    else
+      return "Unknown"
+    end
+  end
+
+  def passenger_name
+    passenger = self.passenger
+    if passenger
+      return passenger.name
+    else
+      return "Unknown"
+    end
+  end
 end
