77a88e1cdf7e62855eb37f488235aa5aef8da790
Grace Shea <grace.m.shea@gmail.com>
2019-04-19 10:00:01 -0700

    text field boxes styled and made larger

---
+++ images/RideShareRails_files/application.self-eaf3d6e6e4d737965f40918f597ceadf971588cfb64312cbbd95030d1b7ce5b3.js
@@ -0,0 +1,59 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+
+
+
+
+
+// THE FOLLOWING IS NO LONGER IN PLAY
+
+// const words = ["Welcome to TRANSport!","(a bigot-free riding app)","Click 'passenger' or 'driver' to see all your options!"];
+// let i = 0;
+// let timer;
+
+// function typingEffect() {
+// 	let word = words[i].split("");
+// 	var loopTyping = function() {
+// 		if (word.length > 0) {
+// 			document.getElementById('word').innerHTML += word.shift();
+// 		} else {
+// 			deletingEffect();
+// 			return false;
+// 		};
+// 		timer = setTimeout(loopTyping, 100);
+// 	};
+// 	loopTyping();
+// };
+
+// function deletingEffect() {
+// 	let word = words[i].split("");
+// 	var loopDeleting = function() {
+// 		if (word.length > 0) {
+// 			word.pop();
+// 			document.getElementById('word').innerHTML = word.join("");
+// 		} else {
+// 			if (words.length > (i + 1)) {
+// 				i++;
+// 			} else {
+// 				i = 0;
+// 			};
+// 			typingEffect();
+// 			return false;
+// 		};
+// 		timer = setTimeout(loopDeleting, 50);
+// 	};
+// 	loopDeleting();
+// };
+
+// typingEffect();
